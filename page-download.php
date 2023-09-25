ec6ca5489e5b430891c45690169417ffe2efcf6e
po6896 <119492836+po6896@users.noreply.github.com>
2023-09-25 23:49:22 +0900

    Create page-download.php

---
+++ page-download.php
@@ -0,0 +1,51 @@
+<?php
+/*
+Template Name: Software Download Page
+*/
+
+get_header(); ?>
+
+<main id="main-content">
+    <div class="software-section">
+        <h1>PhotoMagic</h1>
+        <p><strong>バージョン情報:</strong> Version 2.1.0</p>
+        
+        <h2>ソフトウェアの概要</h2>
+        <p>PhotoMagicは、簡単な操作で写真の加工や編集ができるフリーソフトウェアです。</p>
+        
+        <h2>主な機能</h2>
+        <p>明るさやコントラストの調整、フィルターの適用、リサイズ機能など多数の編集ツールを搭載。</p>
+        
+        <h2>動作環境</h2>
+        <p>Windows 10以上、MacOS 10.13以上</p>
+        
+        <h2>インストール方法</h2>
+        <p>ダウンロード後、ZIPファイルを解凍し、setup.exeを実行してください。</p>
+
+        <h2>利用方法</h2>
+        <p>ソフトウェアを起動後、[ファイル]メニューから写真を選択して編集を開始します。</p>
+        
+        <h2>スクリーンショット</h2>
+        <img src="path_to_screenshot.jpg" alt="PhotoMagicのスクリーンショット" />
+
+        <h2>更新履歴</h2>
+        <p>Version 2.1.0：新しいフィルター追加、バグ修正</p>
+        
+        <h2>ライセンス情報</h2>
+        <p>このソフトウェアはMITライセンスのもとで公開されています。</p>
+        
+        <h2>免責事項</h2>
+        <p>本ソフトウェアを利用した結果生じたいかなる損害についても、開発者は責任を負いません。</p>
+
+        <h2>サポート情報</h2>
+        <p>質問やフィードバックは<a href="mailto:support@example.com">support@example.com</a>までお送りください。</p>
+        
+        <h2>開発者情報</h2>
+        <p>開発者：Taro Yamada / Webサイト：<a href="https://example.com">https://example.com</a></p>
+        
+        <h2>ダウンロード</h2>
+        <a href="path_to_your_app.zip" class="download-button">PhotoMagicをダウンロード</a>
+    </div>
+</main>
+
+<?php get_footer(); ?>
